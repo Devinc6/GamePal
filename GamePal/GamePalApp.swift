377e4ab656cdb069e7fe1c14e1bf8ff902091812
Devin <devin.cassella1@gmail.com>
2025-01-24 10:17:15 -0500

    Initial Commit

---
+++ GamePal/GamePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePalApp.swift
+//  GamePal
+//
+//  Created by Devin Cassella on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct GamePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
